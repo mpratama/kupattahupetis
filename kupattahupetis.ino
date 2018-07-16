15a4b810c60bb214e5310f2d9a0a1c18f0ec4c4f
Arif Mulpratama <arifmul@gmail.com>
2018-07-16 20:52:30 +0900

    optimized function, now can take double value

--- kupattahupetis.ino
+++ kupattahupetis.ino
@@ -8,17 +8,14 @@
 const int pR = 7; //nomor pin relay
 unsigned long detik = 1000L;
 unsigned long menit = detik * 60;
-unsigned long jam = menit * 60;
+//unsigned long jam = menit * 60;
 
-int waktu_nyala = 1; //VARIABEL WAKTU MENYALA
-int waktu_mati = 3; // VARIABEL WAKTU MATI
-
-void w_nyala() {
-  delay(jam * waktu_nyala); //bisa diganti ke menit juga, lihat variable unsigned diatas
+unsigned long w_nyala(double nyala) {
+  return (nyala * menit);
   }
 
-void w_mati() {
-  delay(jam * waktu_mati); //bisa diganti ke menit juga, lihat variable unsigned diatas
+unsigned long w_mati(double mati) {
+  return (mati * menit);
   }
 
 // konfigurasi pin
@@ -30,7 +27,7 @@ void setup() {
 // loop
 void loop() {
   digitalWrite(pR, LOW);   // LOW pompa menjadi nyala karena menggunakan normally open
-  w_nyala();                       // RELAY AKAN MENYALA SELAMA 1 JAM 
+  delay(w_nyala(60));     // masukkan parameter dalam menit
   digitalWrite(pR, HIGH);    // PUTUSKAN JALUR
-  w_mati();                       // RELAY AKAN MATI SELAMA 3 JAM, LALU KEMBALI NYALA 1 JAM, 
+  delay(w_mati(0.2));      // masukkan parameter dalam menit
 }
